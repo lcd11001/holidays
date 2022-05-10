77007eb9ce987ece6dd8d91afeff69edfb95d2eb
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-05-10 16:49:19 +0700

    + render coutries

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Container from '@mui/material/Container';
 
 
 import { getHolidayFree, getHolidays, getCountries, HOLIDAY_TYPE } from './Utils'
-import { Checkbox, FormControlLabel, FormGroup, Tooltip, Typography } from '@mui/material';
+import { Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, ListItemText, MenuItem, OutlinedInput, Select, Tooltip, Typography } from '@mui/material';
 
 
 function App()
 {
   const [holidayTypes, setHolidayTypes] = useState([])
+  const [supportedCountries, setSupportedCountries] = useState([])
   const [countries, setCountries] = useState([])
+  const [countriesID, setCountriesID] = useState([])
+  const [countriesName, setCountriesName] = useState([])
 
   useEffect(() =>
   {
@@ -43,7 +46,7 @@ function App()
     {
       let jsonCountries = JSON.parse(localData)
       console.log('countries from local storage', jsonCountries)
-      setCountries(jsonCountries)
+      setSupportedCountries(jsonCountries)
       return;
     }
     getCountries()
@@ -51,13 +54,13 @@ function App()
       {
         console.log('countries', data)
         localStorage.setItem('countries', JSON.stringify(data, null, 2))
-        setCountries(data)
+        setSupportedCountries(data)
       })
       .catch(err =>
       {
         console.error(err)
       })
-  }, [setCountries])
+  }, [setSupportedCountries])
 
   const onHolidayTypeChanged = (e, checked) =>
   {
@@ -94,12 +97,77 @@ function App()
     )
   }
 
+  const onCountriesChanged = (e) =>
+  {
+    const values = e.target.value
+    let IDs = []
+    let names = []
+
+    values.forEach(data =>
+    {
+      const [ID, name] = data.split('@')
+      IDs.push(ID)
+      names.push(name)
+    });
+
+    console.log('onCountriesChanged', values, 'ID', IDs, 'name', names)
+    setCountries(values)
+    setCountriesID(IDs)
+    setCountriesName(names)
+  }
+
+  const renderSelectedCountries = (selectedIDs) =>
+  {
+    return countriesName.join(', ')
+  }
+
+  const renderCountries = () =>
+  {
+    const LABEL = 'Supported countries'
+    const ITEM_HEIGHT = 48
+    const ITEM_PADDING_TOP = 8
+    const MenuProps = {
+      PaperProps: {
+        style: {
+          maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+          width: 250,
+        },
+      },
+    };
+
+    return (
+      <FormControl sx={{ m: 1, width: 500 }}>
+        <InputLabel>{LABEL}</InputLabel>
+        <Select
+          multiple
+          value={countries}
+          onChange={onCountriesChanged}
+          input={<OutlinedInput label={LABEL} />}
+          renderValue={renderSelectedCountries}
+          MenuProps={MenuProps}
+        >
+          {
+            supportedCountries.map(data => (
+              <MenuItem key={data.uuid} value={`${data['iso-3166']}@${data.country_name}`} >
+                <Checkbox checked={countriesID.indexOf(data['iso-3166']) > -1} />
+                <ListItemText primary={data.country_name} />
+              </MenuItem>
+            ))
+          }
+        </Select>
+      </FormControl>
+    )
+  }
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="xl">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <Typography align={'center'} variant={'h4'} sx={{ padding: 1, fontFamily: 'Oleo Script' }} >Holidays Compare</Typography>
+          {
+            renderCountries()
+          }
           {
             renderHolidayType()
           }
